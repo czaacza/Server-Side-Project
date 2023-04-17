24cb944456a43cd138c50125d68320268b478bb9
Mati <maticzacza@gmail.com>
2023-04-17 13:35:39 +0300

    trying to connect frontend with backend

--- src/app.ts
+++ src/app.ts
@@ -42,23 +42,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req: Request, res: Response) => {
-  Logging.info(`req.user: ${req.user}`);
-  res.render('index', { user: req.user });
-});
-
-app.get('/cart', (req: Request, res: Response) => {
-  res.render('cart', { user: req.user });
-});
-
-app.get('/checkout', (req: Request, res: Response) => {
-  res.render('checkout', { user: req.user });
-});
-
-app.get('/account', (req: Request, res: Response) => {
-  res.render('account', { user: req.user });
-});
-
 app.use('/api', api);
 
 app.use(notFound);
