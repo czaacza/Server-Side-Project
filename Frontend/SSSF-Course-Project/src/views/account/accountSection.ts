c72c71e4066bbd92e8e7c4e1d133b405d9e6d686
Mati <maticzacza@gmail.com>
2023-04-21 13:52:16 +0300

    account page added

--- Frontend/SSSF-Course-Project/src/views/account/accountSection.ts
+++ Frontend/SSSF-Course-Project/src/views/account/accountSection.ts
@@ -1,6 +1,56 @@
 import { User } from '../../interfaces/User';
 
 export default function accountSection(user?: User): string {
-  const modalHtml = ``;
+  const modalHtml = `<div class="container">
+      <h2 class="text-center mt-5 mb-5">Welcome, <span id="username">User</span>!</h2>
+      <div class="row mt-5">
+        <div class="col-md-4 text-center">
+          <img
+            src="img/person1.jpg"
+            alt="Profile picture"
+            id="profilePicture"
+            class="profile-picture"
+          />
+          <input
+            type="file"
+            id="changeProfilePicture"
+            class="form-control mt-2"
+            accept="image/*"
+            style="display: none"
+          />
+          <button class="btn btn-secondary mt-2" id="changePictureBtn">
+            Change Profile Picture
+          </button>
+        </div>
+        <div class="col-md-8">
+          <h3>Edit Account Details</h3>
+          <form id="accountForm">
+            <div class="mb-3">
+              <label for="firstName" class="form-label">First Name</label>
+              <input type="text" class="form-control" id="firstName" required />
+            </div>
+            <div class="mb-3">
+              <label for="lastName" class="form-label">Last Name</label>
+              <input type="text" class="form-control" id="lastName" required />
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email" required />
+            </div>
+            <div class="mb-3">
+              <label for="phoneNumber" class="form-label">Phone Number</label>
+              <input
+                type="tel"
+                class="form-control"
+                id="phoneNumber"
+                required
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">Save Changes</button>
+          </form>
+        </div>
+      </div>
+    </div>
+`;
   return modalHtml;
 }
